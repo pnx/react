8a2853cd59d1b9b2eb57d1926f854ace643c7475
Henrik Hautakoski <henrik@fiktivkod.org>
2013-09-08 16:53:53 +0200

    rand.c: in rand_seed: skip restoring CCTL and CCR.
    this function should be used before CCTL0 and CCR0 is set up for
    other uses.

--- rand.c
+++ rand.c
@@ -49,12 +49,10 @@ int rand(void) {
 int rand_seed(void) {
 
 	int i, j;
-	unsigned _ctl, _cctl, _ccr, r = 0;
+	unsigned _ctl, r = 0;
 
 	/* Save old Timer A control state. */
 	_ctl = TACTL;
-	_cctl = RTACCTL;
-	_ccr = RTACCR;
 
 	RTACCTL = CAP | CM_1 | CCIS_1;	/* Capture mode */
 	TACTL 	= TASSEL_2 | MC_2; 	/* SMCLK (submain), continuous mode */
@@ -83,8 +81,6 @@ int rand_seed(void) {
 
 	/* Restore state */
 	TACTL = _ctl;
-	RTACCTL = _cctl;
-	RTACCR = _ccr;
 
 	return r;
 }
